2acd16a0c0eaacd7616a95f93353e846760b11d6
Nishant Salian <nishant.salian1594@gmail.com>
2022-08-13 18:35:30 -0500

    Create Sum_Words_Sort.py

---
+++ Sum_Words_Sort.py
@@ -0,0 +1,4 @@
+# Alphabets have values a to z [1 to 26]
+# Sort values by length of string and only select those words whose sum is 100
+# Input arr = [10, 'arr','bat','cat', .......] First value is number of strings in the array
+# Return strings with sum 100 ordered by lenght of the string 

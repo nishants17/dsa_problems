64a7521bfca64bb669d2fa4b5d9765c652a6a6e1
Nishant Salian <nishant.salian1594@gmail.com>
2022-08-23 22:27:28 -0500

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,6 +1,30 @@
+# Approach 1: Using Sorted
+# Time Complexity: O(nlogn)
+# Space Complexity: O(nlogn)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if sorted(s) == sorted(t):
             return True
         else:
             return False
+        
+# Approach 2: Using Hash Maps
+# Time Complexity: O(N*M)
+# Space Complexity: O(N*M)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        # Counting string occurences in s 
+        for ele in s:
+            if ele in d:
+                d[ele]+=1
+            else:
+                d[ele] = 1
+        d1 = {}
+        # Counting string occurences in t
+        for ele in t:
+            if ele in d1:
+                d1[ele]+=1
+            else:
+                d1[ele] = 1
+        return d == d1

b0caa79406f8a3ceac692e0b55defdc6319781f7
Nishant Salian <nishant.salian1594@gmail.com>
2022-08-24 08:41:46 -0500

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,18 @@
+# Approach 1: Hash map
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = {}
+        for i in nums:
+            counter[i] = 1+ counter.get(i, 0)
+            if counter[i] > 1:
+                return True
+        return False
+      
+# Approach 2: Using a set
+# Time Complexity: 
+# Space Complexity: 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return not len(set(nums)) == len(nums)
